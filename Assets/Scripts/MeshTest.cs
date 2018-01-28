0d9013ee34be0f60983a216ddb08ba432368ed6b
GuitarmonYz <guitarmonyz@gmail.com>
2018-01-28 10:46:47 -0500

    start circle of fifth stage

---
+++ Assets/Scripts/MeshTest.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Linq;
+
+/// <inheritdoc />
+/// <summary>
+/// Test class for drawing a polygon mesh
+/// </summary>
+public class MeshTest : MonoBehaviour
+{
+	private string[] circleOfFifths = {"C", "G", "D", "A", "E", "B", "F#", "Db", "Ab", "Eb", "Bb", "F"};
+	private Vector2[] circlePos = new Vector2[12];
+	private string[] midiNames = {"C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"};
+	private float radius = 3.56f;
+    private void Start()
+    {
+        for (int i = 0; i < 12; i++) {
+			float angle = findAngle(circleOfFifths[i]);
+			circlePos[i] = new Vector2(radius * Mathf.Sin(Mathf.Deg2Rad * angle), radius * Mathf.Cos(Mathf.Deg2Rad * angle));
+		}
+    }
+	
+	void Update()
+	{
+		if (Input.GetKeyDown("e")){
+			var vertices2D = new Vector2[] {
+				circlePos[0],
+				circlePos[1],
+				circlePos[4],
+				circlePos[5]
+			};
+
+			var vertices3D = System.Array.ConvertAll<Vector2, Vector3>(vertices2D, v => v);
+	
+			// Use the triangulator to get indices for creating triangles
+			var triangulator = new Triangulator(vertices2D);
+			var indices =  triangulator.Triangulate();
+			
+			// Generate a color for each vertex
+			var colors = Enumerable.Range(0, vertices3D.Length)
+				.Select(i => Random.ColorHSV())
+				.ToArray();
+
+			// Create the mesh
+			var mesh = new Mesh {
+				vertices = vertices3D,
+				triangles = indices,
+				colors = colors
+			};
+			
+			mesh.RecalculateNormals();
+			mesh.RecalculateBounds();
+	
+			// Set up game object with mesh;
+			var meshRenderer = gameObject.AddComponent<MeshRenderer>();
+			meshRenderer.material = new Material(Shader.Find("Sprites/Default"));
+			
+			var filter = gameObject.AddComponent<MeshFilter>();
+			filter.mesh = mesh;
+		}
+	}
+	private float findAngle(string noteName){
+		float angle = 0;
+		for (int i = 0; i < circleOfFifths.Length; i++) {
+			if (circleOfFifths[i].Equals(noteName)) {
+				angle = i * 30;
+			}
+		}
+		return angle;
+	}
+	private string noteNum2Name(int noteNum){
+		return this.midiNames[noteNum % 12];
+	}
+}
---
+++ Assets/Scripts/arrowController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+public class arrowController : MonoBehaviour {
+	public GameObject center;
+	private string[] circleOfFifths = {"C", "G", "D", "A", "E", "B", "F#", "Db", "Ab", "Eb", "Bb", "F"};
+	//private Vector2[] circlePos = new Vector2[12];
+	private string[] midiNames = {"C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"};
+	private int rotateCounter = 0;
+	private float speed = 10;
+	private float radius = 3.56f;
+	private string preNote = "C";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		for (int i = 0; i < 128; i++) {
+			if (MidiMaster.GetKeyDown(i)) {
+				string curNote = noteNum2Name(i);
+				rotate(preNote, curNote);
+				preNote = curNote;
+			}
+		}
+
+		// if (rotateCounter < speed) {
+		// 	rotateCounter++;
+		// 	rotate("A", "B", speed);
+		// 	//Debug.Log("rotating");
+		// } else {
+		// 	rotateCounter = 0;
+		// }
+		//rotate("A", "B");
+	}
+
+	private string noteNum2Name(int noteNum){
+		return this.midiNames[noteNum % 12];
+	}
+
+	private void rotate(string startNote, string endNote){
+		float angleToRotate = findAngle(endNote) - findAngle(startNote);
+		transform.RotateAround(center.transform.position, Vector3.back, angleToRotate);
+	}
+
+	private float findAngle(string noteName){
+		float angle = 0;
+		for (int i = 0; i < circleOfFifths.Length; i++) {
+			if (circleOfFifths[i].Equals(noteName)) {
+				angle = (i + 1) * 30;
+			}
+		}
+		return angle;
+	}
+}
